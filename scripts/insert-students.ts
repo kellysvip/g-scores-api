bca4766d2f1f2b7a25cc7855b41e9c3d1fb73fec
kellysvip <110405220+kellysvip@users.noreply.github.com>
2024-11-11 14:36:54 +0700

    fix/insert-scripts: update insert scripts (#3)
    Co-authored-by: Kelly <kelly@MacBook-Pro.lan>

--- scripts/insert-students.ts
+++ scripts/insert-students.ts
@@ -1,7 +1,6 @@
 import { readExcel } from '../src/utils/read-excel';
 import dataSource from '../src/config/ormconfig';
 import { StudentEntity } from '../src/students/student.entity';
-import { StudentSubscriber } from '../src/students/student.subcriber';
 import { getCurrentDate } from '../src/helpers';
 
 type PartialStudentType = Pick<
@@ -34,39 +33,37 @@ export async function insertStudents(filePath, sheetName, dataSource) {
 
         const studentsRepo = dataSource.getRepository(StudentEntity);
 
-        new StudentSubscriber(dataSource);
-
-        await studentsRepo.save(
-            students
-                .map(({
-                    id,
-                    math,
-                    literature,
-                    foreignLanguage,
-                    physics,
-                    chemistry,
-                    biology,
-                    history,
-                    geography,
-                    civicEducation,
-                    foreignLanguageCode,
-                }) => ({
-                    id: id,
-                    math: math ? Number(math) : null,
-                    literature: math ? Number(literature) : null,
-                    foreignLanguage: foreignLanguage ? Number(foreignLanguage) : null,
-                    physics: physics ? Number(physics) : null,
-                    chemistry: chemistry ? Number(chemistry) : null,
-                    biology: biology ? Number(biology) : null,
-                    history: history ? Number(history) : null,
-                    geography: geography ? Number(geography) : null,
-                    civicEducation: civicEducation ? Number(civicEducation) : null,
-                    foreignLanguageCode: foreignLanguageCode ?? null,
-                    createdAt: getCurrentDate(),
-                    updatedAt: getCurrentDate(),
-                }))
-
-        );
+        for (let i = 0; i <= students.length; i += 5000)
+            await studentsRepo.save(
+                students.slice(i, i + 5000)
+                    .map(({
+                        id,
+                        math,
+                        literature,
+                        foreignLanguage,
+                        physics,
+                        chemistry,
+                        biology,
+                        history,
+                        geography,
+                        civicEducation,
+                        foreignLanguageCode,
+                    }) => ({
+                        id: id,
+                        math: math ? Number(math) : null,
+                        literature: math ? Number(literature) : null,
+                        foreignLanguage: foreignLanguage ? Number(foreignLanguage) : null,
+                        physics: physics ? Number(physics) : null,
+                        chemistry: chemistry ? Number(chemistry) : null,
+                        biology: biology ? Number(biology) : null,
+                        history: history ? Number(history) : null,
+                        geography: geography ? Number(geography) : null,
+                        civicEducation: civicEducation ? Number(civicEducation) : null,
+                        foreignLanguageCode: foreignLanguageCode ?? null,
+                        createdAt: getCurrentDate(),
+                        updatedAt: getCurrentDate(),
+                    }))
+            );
 
     } catch (error) {
         throw error;
